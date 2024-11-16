2e043314ffedf40a8bae17cd266e86a750c093b0
praveenvermadev2 <167044212+praveenvermadev2@users.noreply.github.com>
2024-11-16 12:28:46 +0530

    added react paper naviagtion

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,77 +1,74 @@
 import * as React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Text} from 'react-native';
+import {BottomNavigation} from 'react-native-paper';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import SettingsScreen from './Settings';
 import ChatListScreen from './ChatList';
 import Channels from './Channels';
 import SupportChat from './SupportChat';
 import Contacts from './Contacts';
 
-const Tab = createBottomTabNavigator();
-11;
+type RouteType = {
+  key: string;
+  title: string;
+  icon: string;
+};
 
-const HomeTabs = () => {
+const SupportChatRoute = () => <SupportChat />;
+const ChatListRoute = () => <ChatListScreen />;
+const ChannelsRoute = () => <Channels />;
+const ContactsRoute = () => <Contacts />;
+
+const HomeTabs: React.FC = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState<RouteType[]>([
+    {key: 'supportChat', title: 'SupportChat', icon: 'support-agent'},
+    {key: 'chatList', title: 'ChatList', icon: 'chat'},
+    {key: 'channels', title: 'Channels', icon: 'groups'},
+    {key: 'contacts', title: 'Contacts', icon: 'contacts'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    supportChat: SupportChatRoute,
+    chatList: ChatListRoute,
+    channels: ChannelsRoute,
+    contacts: ContactsRoute,
+  });
+
+  const renderIcon: React.FC<{
+    route: RouteType;
+    color: string;
+  }> = ({route, color}) => (
+    <MaterialIcons name={route.icon} size={25} color={color} />
+  );
+
+  const renderLabel: React.FC<{
+    route: RouteType;
+    focused: boolean;
+    color: string;
+  }> = ({route, focused, color}) => (
+    <Text
+      style={{
+        textAlign: 'center',
+        color: focused ? '#5B8E1E' : color,
+        fontWeight: '600',
+        fontSize: 12,
+        marginTop: 2,
+      }}>
+      {route.title}
+    </Text>
+  );
+
   return (
-    <Tab.Navigator
-      initialRouteName="SupportChat"
-      screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
-          let iconName;
-
-          if (route.name === 'SupportChat') {
-            return (
-              <MaterialIcons name={'support-agent'} size={size} color={color} />
-            );
-          } else if (route.name === 'ChatList') {
-            return <MaterialIcons name={'chat'} size={size} color={color} />;
-          } else if (route.name == 'Channels') {
-            return <MaterialIcons name={'groups'} size={size} color={color} />;
-          } else
-            return (
-              <MaterialIcons name={'contacts'} size={size} color={color} />
-            );
-        },
-        tabBarActiveTintColor: '#5B8E1E',
-        tabBarInactiveTintColor: 'gray',
-        tabBarStyle: {
-          height: '8%',
-        },
-
-        tabBarLabel: ({focused, color}) => (
-          <Text
-            style={{
-              color: focused ? '#5B8E1E' : color,
-              fontWeight: '600',
-              fontSize: 12,
-            }}>
-            {route.name}
-          </Text>
-        ),
-      })}>
-      <Tab.Screen
-        name="SupportChat"
-        component={SupportChat}
-        options={{headerShown: false}}
-      />
-      <Tab.Screen
-        name="ChatList"
-        component={ChatListScreen}
-        options={{headerShown: false}}
-      />
-
-      <Tab.Screen
-        name="Channels"
-        component={Channels}
-        options={{headerShown: false}}
-      />
-      <Tab.Screen
-        name="Contacts"
-        component={Contacts}
-        options={{headerShown: false}}
-      />
-    </Tab.Navigator>
+    <BottomNavigation<RouteType>
+      navigationState={{index, routes}}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+      renderIcon={renderIcon}
+      renderLabel={renderLabel}
+      activeColor="#5B8E1E"
+      inactiveColor="gray"
+      barStyle={{backgroundColor: '#fff', height: 'auto'}}
+    />
   );
 };
 
